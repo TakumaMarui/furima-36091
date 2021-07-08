da55ab82ce5444ba3b1b071db3c2acc2a5e8314e
TakumaMarui <circle.i.014@gmail.com>
2021-07-08 22:16:58 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,18 @@ class Item < ApplicationRecord
   belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_days
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }, numericality: true
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category
+    validates :condition
+    validates :shipping_cost
+    validates :prefecture
+    validates :shipping_days
+  end
 end
