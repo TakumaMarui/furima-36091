cf1c7316647d488dd6f12fcfdfb50ded1a374453
TakumaMarui <circle.i.014@gmail.com>
2021-07-09 16:17:42 +0900

    商品出品機能の修正3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,27 +27,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it "category_idが1では保存できないこと" do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it "condition_idが1では保存できないこと" do
-        @item.condition_id = "1"
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
       it "shipping_cost_idが1では保存できないこと" do
-        @item.shipping_cost_id = "1"
+        @item.shipping_cost_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping cost must be other than 1")
       end
       it "prefecture_idが1では保存できないこと" do
-        @item.prefecture_id = "1"
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it "shipping_days_idが1では保存できないこと" do
-        @item.shipping_days_id = "1"
+        @item.shipping_days_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping days must be other than 1")
       end
@@ -57,12 +57,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it "priceが299以下では保存できないこと" do
-        @item.price = "299"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
       it "priceが10,000,000以上では保存できないこと" do
-        @item.price = "10000000"
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
