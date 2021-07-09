44ec014a6f1fc07698e35aaa58fd9c979df68a22
TakumaMarui <circle.i.014@gmail.com>
2021-07-09 10:39:54 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,12 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_days
+  belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :image
@@ -16,10 +15,10 @@ class Item < ApplicationRecord
     validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }, numericality: true
   end
   with_options numericality: { other_than: 1 } do
-    validates :category
-    validates :condition
-    validates :shipping_cost
-    validates :prefecture
-    validates :shipping_days
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_days_id
   end
 end
