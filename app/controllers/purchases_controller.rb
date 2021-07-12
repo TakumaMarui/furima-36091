9a14944cf8aaeedf3af5a54aa980397bbed63004
TakumaMarui <circle.i.014@gmail.com>
2021-07-12 17:07:18 +0900

    商品購入ページの編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -17,6 +17,6 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_params
-    params.require(:purchase_residence).permit(:postal_code, :prefecture_id, :city, :home_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_residence).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
