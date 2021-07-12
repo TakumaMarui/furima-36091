48184974d13eca07ae5674efab0c59cfe27aace5
TakumaMarui <circle.i.014@gmail.com>
2021-07-12 15:10:24 +0900

    purchasesコントローラーの編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,22 @@
 class PurchasesController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @purchase_residence = PurchaseResidence.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @purchase_residence = PurchaseResidence.new(purchase_params)
+    if @purchase_residence.valid?
+      @purchase_residence.save
+      redirect_to root_path 
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_residence).permit(:postal_code, :prefecture_id, :city, :home_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
