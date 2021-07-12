ecf412fc68996451ca77d2e18e2af8cdcac17614
TakumaMarui <circle.i.014@gmail.com>
2021-07-12 14:46:22 +0900

    PurchaseResidenceクラスの定義とバリデーションの修正

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -2,12 +2,4 @@ class Residence < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :purchase
-
-  with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :city
-    validates :house_number
-    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
-  end
-  validates :prefecture_id, numericality: { other_than: 1 }
 end
