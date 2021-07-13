287e643a4acce8fb41bddfe9b1b6ea4dc2ba846f
TakumaMarui <circle.i.014@gmail.com>
2021-07-13 12:33:08 +0900

    クレジットカード機能の導入

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,13 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
   def index
-    @item = Item.find(params[:item_id])
     @purchase_residence = PurchaseResidence.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_residence = PurchaseResidence.new(purchase_params)
     if @purchase_residence.valid?
+      pay_item
       @purchase_residence.save
       redirect_to root_path 
     else
@@ -17,6 +17,19 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_params
-    params.require(:purchase_residence).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_residence).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/purchase_residence.rb
+++ app/models/purchase_residence.rb
@@ -1,6 +1,6 @@
 class PurchaseResidence
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :item_id
@@ -9,6 +9,7 @@ class PurchaseResidence
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1 }
 
